46d20b368f519635ed619ea49bd47233ed2c0451
Daniel Wohlrath <wohlrath.daniel@seznam.cz>
2025-05-31 09:27:26 +0300

    Add orchestrator and utils

---
+++ backend/app/agents/tools.py
@@ -0,0 +1,8 @@
+tools = {
+    "get_schema": scan_database_schema,
+    "generate_sql": generate_sql_query,
+    "run_sql": run_query,
+    "plot_chart": plot_result,
+    "explain": explain_result
+}
+
